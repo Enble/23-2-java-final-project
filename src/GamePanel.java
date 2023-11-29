1ed70ae50e96af5efc544733123a03859da11dfc
enble <enble_777@naver.com>
2023-11-29 22:08:57 +0900

    feat: GameFrame 및 기본기능 생성

---
+++ src/GamePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        add(new JLabel("GamePanel"));
+    }
+
+}
---
+++ src/LoginPanel.java
@@ -0,0 +1,8 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LoginPanel extends JPanel {
+    public LoginPanel() {
+        add(new JLabel("LoginPanel"));
+    }
+}
